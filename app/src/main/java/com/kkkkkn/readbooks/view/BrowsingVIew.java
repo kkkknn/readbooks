16bbbfdecda9d2e77a3ecb38a5e4ec87996cec9e
kkkkkn <1185553557@qq.com>
2020-08-14 17:00:00 +0800

    简化相关文件，修改图书浏览页

--- app/src/main/java/com/kkkkkn/readbooks/view/BrowsingVIew.java
+++ app/src/main/java/com/kkkkkn/readbooks/view/BrowsingVIew.java
@@ -39,40 +39,7 @@ public class BrowsingVIew extends View {
 
 
     @Override
-    public void draw(Canvas canvas) {
-        Bitmap bitmap= BitmapFactory.decodeResource(getResources(), R.drawable.browsingview);
-        Bitmap bitmap2=Bitmap.createScaledBitmap(bitmap,getWidth(),getHeight(),true);
-        Paint paint=new Paint();
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawBitmap(bitmap2,0,0,paint);
-        TextPaint point=new TextPaint();
-        point.setTextSize(40f);
-        point.setColor(Color.BLACK);
-        point.setAntiAlias(true);
-        int height=getStatusBarHeight(getContext());
-        String str="生命中的痛苦就像是盐的咸味一样，就这么多。而我们所能感受和体验的程度，取决于我们将它放在多大的容器里。你的心量越小，烦恼就多了，心量大了，能承受的就多了，生活就不那么苦了。用敞亮的心去看待世界，世界就是闪闪发光的，相反，用阴暗的心去看待世界，世界就永无发光之日。";
-        float size=point.getTextSize();
-        char[] arr=str.toCharArray();
-        int num=Math.round(getWidth()/size);
-        int line=arr.length/num;
-        for (int i = 0; i < (line+1); i++) {
-            int count=arr.length-i*num;
-            if(count<num){
-                num=count;
-            }else{
-                count=num;
-            }
-            canvas.drawText(arr,i*num,count,0,size*i+height+1,point);
-        }
-        Log.i("TAG", "onDraw: "+num);
-        super.draw(canvas);
-    }
-
-    @Override
     protected void onDraw(Canvas canvas) {
-
-
         /*StaticLayout layoutopen = new StaticLayout(str,  point,  getWidth() , Layout.Alignment.ALIGN_NORMAL, 1.0F, 0.0F, true);
         //这里的参数300，表示字符串的长度，当满300时，就会换行，也可以使用“\r\n”来实现换行
         canvas.save();
@@ -80,8 +47,7 @@ public class BrowsingVIew extends View {
         layoutopen.draw(canvas);
         canvas.restore();*/
         super.onDraw(canvas);
-        scroller.startScroll(getScrollX(),0,0,200);
-        invalidate();
+        testDraw(canvas);
     }
 
 
@@ -90,12 +56,36 @@ public class BrowsingVIew extends View {
         return false;
     }
 
+    private void testDraw(Canvas canvas){
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.browsingview);
+        Bitmap bitmap2 = Bitmap.createScaledBitmap(bitmap, getWidth(), getHeight(), true);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawBitmap(bitmap2, 0, 0, paint);
+        TextPaint point = new TextPaint();
+        point.setTextSize(40f);
+        point.setColor(Color.BLACK);
+        point.setAntiAlias(true);
+        int height = getStatusBarHeight(getContext());
+        String str = "生命中的痛苦就像是盐的咸味一样，就这么多。而我们所能感受和体验的程度，取决于我们将它放在多大的容器里。你的心量越小，烦恼就多了，心量大了，能承受的就多了，生活就不那么苦了。用敞亮的心去看待世界，世界就是闪闪发光的，相反，用阴暗的心去看待世界，世界就永无发光之日。";
+        //将字体大小向下取整
+        int size = (int) point.getTextSize();
+        char[] arr = str.toCharArray();
+        int num = getWidth() / size;
+        //行数需要向上取整， 不能强制转换
+        int line = arr.length / num == 0 ? arr.length / num : arr.length / num + 1;
+        for (int i = 0; i < line; i++) {
+            int count = arr.length - i * num;
+            canvas.drawText(arr, i * num, Math.min(count, num), 0, size * i + height, point);
+        }
+        Log.i("TAG", "onDraw: " + num + "||arr" + arr.length + "||line" + line);
+    }
 
-    public int getStatusBarHeight(Context context) {
+    private int getStatusBarHeight(Context context) {
         Resources resources = context.getResources();
         int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
-        int height = resources.getDimensionPixelSize(resourceId);
-        return height;
+        return resources.getDimensionPixelSize(resourceId);
     }
 
 }
