baad4ff89bb52ef7188aa5ab38ada72a20289119
kkkkkn <1185553557@qq.com>
2020-11-24 17:47:52 +0800

    update

--- app/src/main/java/com/kkkkkn/readbooks/util/sqlite/inf/SQLiteUtil.java
+++ app/src/main/java/com/kkkkkn/readbooks/util/sqlite/inf/SQLiteUtil.java
@@ -1,5 +1,7 @@
 package com.kkkkkn.readbooks.util.sqlite.inf;
 
+import com.kkkkkn.readbooks.util.sqlite.entity.SQLBookInfo;
+
 import java.util.ArrayList;
 
 public interface SQLiteUtil {
@@ -9,5 +11,6 @@ public interface SQLiteUtil {
     public boolean deleteBook();
     //查询下载图书数据
     public ArrayList<String> selectBook();
-
+    //修改图书数据
+    public boolean UpdateBook(SQLBookInfo sqlBookInfo);
 }
